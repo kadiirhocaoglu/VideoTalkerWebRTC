0e129d927fba96ee5cf92c60df4e6b17db76c60e
kadiirhocaoglu <kadiirhocaoglu@hotmail.com>
2024-09-17 10:52:35 +0300

    handle buttons

--- video-talker-client/src/pages/dashboard/components/incomingCallDialog/incomingCallDialog.js
+++ video-talker-client/src/pages/dashboard/components/incomingCallDialog/incomingCallDialog.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './incomingCallDialog.css';
 
-const incomingCallDialog = ({ onAccept, onReject }) => {
+const incomingCallDialog = () => {
+  const handleAcceptButtonPressed = () => {
+    // accept the call
+  };
+
+  const handleRejectButtonPressed = () => {
+    // accept the call
+  };
+
   return (
     <div className="incoming-call-container">
       <h2>Incoming Call...</h2>
       <div className="button-group">
-        <button className="accept-button" onClick={onAccept}>
+        <button className="accept-button" onClick={handleAcceptButtonPressed}>
           Accept
         </button>
-        <button className="reject-button" onClick={onReject}>
+        <button className="reject-button" onClick={handleRejectButtonPressed}>
           Reject
         </button>
       </div>
